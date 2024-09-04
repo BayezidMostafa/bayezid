28e1b4694d0188311fcb27aaa03cc217f54e91b7
BayezidMostafa <hiperx1@gmail.com>
2024-09-04 11:18:24 +0600

    rem ch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
@@ -11,6 +10,7 @@ import Cursor from "@/components/common/Cursor/Cursor";
 import { Toaster } from "react-hot-toast";
 import CTA from "@/components/common/CTA/CTA";
 import Loader from "@/components/common/Loader/Loader";
+import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 const ubuntu = Ubuntu({
@@ -20,29 +20,17 @@ const ubuntu = Ubuntu({
   display: "swap",
 });
 
+// export const metadata: Metadata = {
+//   title: "Bayezid Mostafa",
+//   description: "Portfolio created by Bayezid Mostafa",
+// };
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Inject Tawk.to script dynamically
-    const tawkScript = document.createElement("script");
-    tawkScript.async = true;
-    tawkScript.src = "https://embed.tawk.to/66d7ea3e50c10f7a00a3c04c/1i6tmm17u";
-    tawkScript.charset = "UTF-8";
-    tawkScript.setAttribute("crossorigin", "*");
-    const scriptTag = document.getElementsByTagName("script")[0];
-    scriptTag.parentNode?.insertBefore(tawkScript, scriptTag);
-
-    return () => {
-      // Cleanup the script if the component unmounts
-      tawkScript.remove();
-    };
-  }, []);
-
   return (
     <html lang="en">
       <head>
